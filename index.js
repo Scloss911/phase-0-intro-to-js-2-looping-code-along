2ca9f52afc2e03516953571f8a80709aeeababa2
Sandra Clossey <ssclossey@gmail.com>
2022-11-02 23:24:19 -0700

    commit tree adf8c9b7f10c30479a9b110c1612e78f10932d3b

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const names = ["Giadalupe", "Ollie", "Aki"];
+const event = 'surprise';
+
+function writeCards(names, event) {
+    let newGreeting = [];
+    for (let x = 0; x < names.length; x++) {
+        newGreeting.push (`Thank you, ${names[x]}, for the wonderful ${event} gift!`);
+    }
+    return newGreeting;
+}
+console.log(writeCards(names, event));
+
+function countDown(positiveNumber){
+    while(positiveNumber >= 0) {
+        console.log(positiveNumber);
+        positiveNumber--;
+
+    }
+}
+console.log(countDown(10))
+
